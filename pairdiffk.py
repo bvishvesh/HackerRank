0ab55a9eda2f1bbfd1e38e77a2cecb0872454a8e
VISHVESH BHAVSAR <46962441+bvishvesh@users.noreply.github.com>
2020-08-02 18:38:05 +0530

    Update pairdiffk.py

--- pairdiffk.py
+++ pairdiffk.py
@@ -1,6 +1,11 @@
+import datetime
+start_time=datetime.datetime.now()
 ar,k=sorted(set(map(int,input().rstrip().split()))),int(input())
 #print(ar)
 #temp=set([])
 for i in ar:
     if abs(i-k) in ar:
         print((i,abs(i-k)),end=" ")
+end_time = datetime.datetime.now()
+total=end_time-start_time
+print("{:.3f}".format(total.total_seconds()*1000))
