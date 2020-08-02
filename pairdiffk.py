ac4f3b4fe251d03d8e557b7f306c327ba894c0b0
VISHVESH BHAVSAR <46962441+bvishvesh@users.noreply.github.com>
2020-08-02 18:23:08 +0530

    Update pairdiffk.py

--- pairdiffk.py
+++ pairdiffk.py
@@ -1,7 +1,6 @@
 ar,k=sorted(set(map(int,input().rstrip().split()))),int(input())
 #print(ar)
-temp=set([])
+#temp=set([])
 for i in ar:
     if abs(i-k) in ar:
-        temp.add((abs(i-k),i))
-print(*temp)
+        print((i,abs(i-k)),end=" ")
